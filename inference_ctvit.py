b8357c73031d5623c4fee5e6e87c73cba40b0727
Abit Ahmed <abit@Abits-Mac-mini.local>
2024-10-25 07:36:58 -0700

    fork and modify for free colab's first commit

---
+++ inference_ctvit.py
@@ -0,0 +1,32 @@
+import torch
+from transformer_maskgit import CTViT 
+from transformer_maskgit.ctvit_inference import CTVIT_inf
+
+
+ctvit = CTViT(
+dim = 64,
+codebook_size = 1024,
+image_size = 64,
+patch_size = 16,
+temporal_patch_size = 2,
+spatial_depth = 4,
+temporal_depth = 4,
+dim_head = 8,
+heads = 4
+)
+ctvit.load('pretrained_models/ctvit_pretrained.pt')
+
+vit_infer = CTVIT_inf(
+    ctvit,
+    folder = 'example_data_valid_ctvit',
+    batch_size = 1,
+    results_folder="ctvit_inference",
+    grad_accum_every = 1,
+    train_on_images = False,  # you can train on images first, before fine tuning on video, for sample efficiency
+    use_ema = False,          # recommended to be turned on (keeps exponential moving averaged ctvit) unless if you don't have enough resources
+    num_train_steps = 1,
+    num_frames=2
+)
+
+vit_infer.infer()               # reconstructions and checkpoints will be saved periodically to ./results
+
---
+++ train_transformer.py
@@ -0,0 +1,67 @@
+from transformer_maskgit import CTViT, MaskGit, MaskGITTransformer
+from transformer_maskgit.videotextdataset import VideoTextDataset
+from transformer_maskgit.train_transformer import TransformerTrainer
+from torch.utils.data import Dataset, DataLoader, random_split
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+import torch.optim as optim
+import torch.multiprocessing as mp
+import os
+
+def cycle(dl):
+    while True:
+        for data in dl:
+            yield data
+            
+def train():
+    # set up distributed training
+
+    ctvit = CTViT(
+        dim = 64,
+        codebook_size = 1024,
+        image_size = 64,
+        patch_size = 16,
+        temporal_patch_size = 2,
+        spatial_depth = 4,
+        temporal_depth = 4,
+        dim_head = 8,
+        heads = 4
+    )
+
+
+    # Load the pre-trained weights
+
+    pretrained_ctvit_path = 'pretrained_models/ctvit_pretrained.pt'
+    ctvit.load(pretrained_ctvit_path)
+
+    maskgit = MaskGit(
+        num_tokens=1024,
+        max_seq_len=10000,
+        dim=64,
+        dim_context=768,
+        depth=2,
+    )
+   
+    transformer_model = MaskGITTransformer(
+        ctvit=ctvit,
+        maskgit=maskgit
+    )
+    batch_size=1
+    #transformer_model.load('pretrained_models/transformer_pretrained.pt')
+
+    # initialize DDP
+    trainer = TransformerTrainer(
+        transformer_model,
+        num_train_steps=100000000,
+        batch_size=1,
+        pretrained_ctvit_path='pretrained_models/ctvit_pretrained.pt',
+        results_folder="transformer_train"
+    )
+
+
+    trainer.train()
+
+if __name__ == '__main__':
+    # set up multiprocessing
+    train()
